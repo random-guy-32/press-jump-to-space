8e4bfc3b3314e2d40a8c63f63483278c3909a5bc
Random <l>
2017-07-10 17:42:43 +0200

    test

--- src/pressjumptospace/entity/projectile/SmallBullet.java
+++ src/pressjumptospace/entity/projectile/SmallBullet.java
@@ -3,7 +3,6 @@ package pressjumptospace.entity.projectile;
 import pressjumptospace.entity.meta.CollisionReaction;
 import pressjumptospace.entity.meta.Projectile;
 
-
 public class SmallBullet extends Projectile {
     public SmallBullet() {
         super(0, 0, new boolean[] {true, true, true, true}, new int[] {0, 1, 1, 1}, new boolean[] {true, false, false, false}, 2, 2, 7, 7, "entity/small-bullet.png", (byte) 0, false, 1, 1, false, false, false, false, CollisionReaction.DEFAULT, 9f);
